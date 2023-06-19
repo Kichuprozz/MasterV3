23bbc87b0046700b1a7ca855040a1f4a4cca657b
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2023-06-19 19:00:13 +0530

    Update mainfilter.py

--- plugins/mainfilter.py
+++ plugins/mainfilter.py
@@ -1395,11 +1395,6 @@ async def start(client, message):
             InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIFd2SQVaJIUtgWBcs-eiebCASh2vCMAAInAQACyJRkFOI9YoCRRKaaHgQ")
-        await asyncio.sleep(1)
-        await m.delete() 
-        await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
@@ -1444,11 +1439,7 @@ async def start(client, message):
             InlineKeyboardButton('ℹ️ ʜᴇʟᴩ', callback_data='help2'),
             InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIFd2SQVaJIUtgWBcs-eiebCASh2vCMAAInAQACyJRkFOI9YoCRRKaaHgQ")
-        await asyncio.sleep(1)
-        await m.delete()                                    
+        reply_markup = InlineKeyboardMarkup(buttons)                                    
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
