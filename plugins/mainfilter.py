dd6c045f02ac1859d1260f8ecebdf0d02281a1e1
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2023-06-19 19:04:24 +0530

    Update mainfilter.py

--- plugins/mainfilter.py
+++ plugins/mainfilter.py
@@ -1395,6 +1395,7 @@ async def start(client, message):
             InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
